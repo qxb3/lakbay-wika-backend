4290c5faa38cef58b86b6fe04d3302a072dfb5a2
qxbthree <qxbthree@gmail.com>
2025-05-13 20:06:27 +0800

    /request-uuid and /login-admin

--- src/main/java/org/lakbaywika/capstone/Main.java
+++ src/main/java/org/lakbaywika/capstone/Main.java
@@ -11,10 +11,19 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.io.PipedReader;
+import java.sql.*;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, ParseException {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/lakbaywika";
+    static final String DB_USER = "lakbay";
+    static final String DB_PASS = "pass123";
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, ParseException, SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+
         String modelPath = Main.class.getClassLoader().getResource("vosk-model-small-en-us-0.15").getPath();
         File modelFolder = new File(modelPath);
         Model model = new Model(modelFolder.getPath());
@@ -67,6 +76,43 @@ public class Main {
             recognizer.close();
 
             ctx.result(result);
+        });
+
+        app.get("/request-uuid", ctx -> {
+            String uuid = UUID.randomUUID().toString();
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (id) VALUES (?)");
+            statement.setString(1, uuid);
+
+            int affected = statement.executeUpdate();
+
+            if (affected > 0) {
+                ctx.status(200).result(uuid);
+            } else {
+                ctx.status(500).result("Failed to give uuid, Please Try again");
+            }
+        });
+
+        app.post("/admin-login", ctx -> {
+            String username = ctx.queryParam("username");
+            String password = ctx.queryParam("password");
+
+            if (username == null || password == null) {
+                ctx.status(500).result("username & password is required");
+                return;
+            }
+
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM admins WHERE username = ? AND password = ?");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.executeQuery();
+
+            ResultSet result = statement.getResultSet();
+            if (result.next()) {
+                ctx.status(200).result(username);
+            } else {
+                ctx.status(404).result("Invalid username or password");
+            }
         });
     }
 }
